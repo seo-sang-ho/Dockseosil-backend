ce0d249030ee189e7400a0821f174f0f6d433f11
seo-sang-ho <rocter2049@naver.com>
2025-03-30 03:11:35 +0900

    enviroment setting

---
+++ src/main/java/ChatProject/DockSeosil/DockSeosilApplication.java
@@ -0,0 +1,13 @@
+package ChatProject.DockSeosil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockSeosilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockSeosilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ChatProject/DockSeosil/DockSeosilApplicationTests.java
@@ -0,0 +1,13 @@
+package ChatProject.DockSeosil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockSeosilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
