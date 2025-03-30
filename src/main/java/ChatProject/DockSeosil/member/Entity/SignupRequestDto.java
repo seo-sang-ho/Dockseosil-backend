2809c24172a806514253c213bc476b685c27cf98
seo-sang-ho <rocter2049@naver.com>
2025-03-30 23:37:34 +0900

    feat: 회원가입 api 구현

---
+++ src/main/java/ChatProject/DockSeosil/member/Entity/SignupRequestDto.java
@@ -0,0 +1,12 @@
+package ChatProject.DockSeosil.member.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignupRequestDto {
+	private String username;
+	private String password;
+	private String email;
+}
