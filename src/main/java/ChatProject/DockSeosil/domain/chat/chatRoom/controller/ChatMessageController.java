631d925800ac7967c507ae609666f8019d32d3d7
seo-sang-ho <rocter2049@naver.com>
2025-05-13 14:51:28 +0900

    feat: thymeleaf && 채팅 기초 틀 작성

--- src/main/java/ChatProject/DockSeosil/domain/chat/chatRoom/controller/ChatMessageController.java
+++ src/main/java/ChatProject/DockSeosil/domain/chat/chatRoom/controller/ChatMessageController.java
@@ -3,18 +3,25 @@ package ChatProject.DockSeosil.domain.chat.chatRoom.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/chat/room")
 public class ChatMessageController {
 
-	@GetMapping("/chat/room/{roomId}")
+	@GetMapping("/{roomId}")
 	@ResponseBody
 	public String showRoom(
 			@PathVariable final Long roomId,
 			@RequestParam(defaultValue = "NoName") final String writerName
  		){
 		return "%d번 채팅방입니다. writer : %s".formatted(roomId,writerName);
+	}
+
+	@GetMapping("/make")
+	public String showMake(){
+		return "chat/chatRoom/make";
 	}
 }
