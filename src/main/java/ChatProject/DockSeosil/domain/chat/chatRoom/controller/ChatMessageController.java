f30ba2de618d3ea0fbdfa301b3cfbc7dfc9305bd
seo-sang-ho <rocter2049@naver.com>
2025-05-13 14:38:40 +0900

    feat: @PathVariable roomId

--- src/main/java/ChatProject/DockSeosil/domain/chat/chatRoom/controller/ChatMessageController.java
+++ src/main/java/ChatProject/DockSeosil/domain/chat/chatRoom/controller/ChatMessageController.java
@@ -2,14 +2,19 @@ package ChatProject.DockSeosil.domain.chat.chatRoom.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ChatMessageController {
 
-	@GetMapping("/chat/room/1")
+	@GetMapping("/chat/room/{roomId}")
 	@ResponseBody
-	public String showRoom(){
-		return "1번 채팅방입니다.";
+	public String showRoom(
+			@PathVariable final Long roomId,
+			@RequestParam(defaultValue = "NoName") final String writerName
+ 		){
+		return "%d번 채팅방입니다. writer : %s".formatted(roomId,writerName);
 	}
 }
